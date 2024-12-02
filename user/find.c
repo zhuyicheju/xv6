2fb5f7358aa334d0807670f8319aa97dda433064
zhuyicheju <1279275025@qq.com>
2024-12-02 21:07:16 +0800

    optimize find

--- user/find.c
+++ user/find.c
@@ -24,53 +24,110 @@ char* fmtname(char *path)
 }
 void find(char* path, char* name){
     char buf[512], *p;
-    int fd;
-    struct dirent de;
     struct stat st;
-    if((fd = open(path, 0)) < 0){
-        fprintf(2, "find: cannot open %s\n",path);
+
+    if(stat(path, &st) < 0){
+        printf("find: cannot stat %s\n", buf);
         return;
     }
-    if(fstat(fd, &st) < 0){
-        fprintf(2, "find: cannot stat %s\n", path);
-        close(fd);
-        return;
-    }
+
     switch(st.type){
         case T_FILE:
             if(strcmp(fmtname(path), name) == 0)
                 printf("%s %d %d %l\n", path, st.type, st.ino, st.size);
-            break;
+        break;
+
         case T_DIR:
+            int fd;
+            struct dirent de;
+
+            if((fd = open(path, 0)) < 0){
+                fprintf(2, "find: cannot open %s\n",path);
+                return;
+            }
+
+            if(fstat(fd, &st) < 0){
+                fprintf(2, "find: cannot stat %s\n", path);
+                close(fd);
+                return;
+            }
+
             if(strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf)){
                 printf("find: path too long\n");
                 break;
             }
+
             strcpy(buf, path);
             p = buf + strlen(buf);
             *p++ = '/';
+
             while(read(fd, &de, sizeof(de)) == sizeof(de)){
                 if(de.inum == 0)
                     continue;
+
                 memmove(p, de.name, DIRSIZ);
                 p[DIRSIZ] = 0;
+
                 if(stat(buf, &st) < 0){
                     printf("find: cannot stat %s\n", buf);
                     continue;
                 }
+
                 if(strcmp(fmtname(buf), ".") == 0 || strcmp(fmtname(buf), "..") == 0) continue;
+                //This can be optimized
+
                 if(strcmp(fmtname(buf), name) == 0)
                     printf("%s %d %d %l\n", buf, st.type, st.ino, st.size);
+                
                 if(st.type == T_DIR)
                     find(buf, name);
             }
+        break;
     }
-    close(fd);
+    // if((fd = open(path, 0)) < 0){
+    //     fprintf(2, "find: cannot open %s\n",path);
+    //     return;
+    // }
+    // if(fstat(fd, &st) < 0){
+    //     fprintf(2, "find: cannot stat %s\n", path);
+    //     close(fd);
+    //     return;
+    // }
+    // switch(st.type){
+    //     case T_FILE:
+    //         if(strcmp(fmtname(path), name) == 0)
+    //             printf("%s %d %d %l\n", path, st.type, st.ino, st.size);
+    //         break;
+    //     case T_DIR:
+    //         if(strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf)){
+    //             printf("find: path too long\n");
+    //             break;
+    //         }
+    //         strcpy(buf, path);
+    //         p = buf + strlen(buf);
+    //         *p++ = '/';
+    //         while(read(fd, &de, sizeof(de)) == sizeof(de)){
+    //             if(de.inum == 0)
+    //                 continue;
+    //             memmove(p, de.name, DIRSIZ);
+    //             p[DIRSIZ] = 0;
+    //             if(stat(buf, &st) < 0){
+    //                 printf("find: cannot stat %s\n", buf);
+    //                 continue;
+    //             }
+    //             if(strcmp(fmtname(buf), ".") == 0 || strcmp(fmtname(buf), "..") == 0) continue;
+    //             if(strcmp(fmtname(buf), name) == 0)
+    //                 printf("%s %d %d %l\n", buf, st.type, st.ino, st.size);
+    //             if(st.type == T_DIR)
+    //                 find(buf, name);
+    //         }
+    // }
+    // close(fd);
 }
 
 int main (int argc, char* argv[]){
     if(argc != 3){
-        fprintf("find: need 2 argv\n");
+        fprintf(2,"find: need 2 argv\n");
         exit(1);
     }
     find(argv[1],argv[2]);
